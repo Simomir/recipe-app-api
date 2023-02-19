ef60b94188a0e0bb336f1b8ab5bbe4e9d214fa70
Simeon Shopov <simeon.t.shopov@gmail.com>
2023-02-19 15:43:02 +0200

    drf spectacular setup for auto schema of the api

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd party apps
     'rest_framework',
+    'drf_spectacular',
     # own apps
     'core',
 ]
@@ -133,3 +134,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
